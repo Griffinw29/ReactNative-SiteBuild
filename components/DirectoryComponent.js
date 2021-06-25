9ad45971168d891ec1c6bb852da7b61864fecff0
Griffinw29 <55906469+Griffinw29@users.noreply.github.com>
2021-06-24 22:04:45 -0400

    added redux loading message with animations

--- components/DirectoryComponent.js
+++ components/DirectoryComponent.js
@@ -1,9 +1,9 @@
 import React, { Component } from "react";
-import { FlatList } from "react-native";
+import { FlatList, View } from "react-native";
 import { Tile } from "react-native-elements";
 import { connect } from "react-redux";
 import { baseUrl } from "../shared/baseUrl";
-
+import Loading from "./LoadingComponent";
 const mapStateToProps = (state) => {
   return {
     campsites: state.campsites,
@@ -29,6 +29,12 @@ class Directory extends Component {
       );
     };
 
+    if (this.props.campsites.isLoading) {
+      return <Loading />;
+    }
+    if (this.props.campsites.errMess) {
+      return <View>{props.campsites.errMess}</View>;
+    }
     return (
       <FlatList
         data={this.props.campsites.campsites}
